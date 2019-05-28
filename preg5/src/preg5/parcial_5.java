3747fe4d4564b5eab79ff7ce8d17b4f7c9a1da65
Montesinos <Montesinos@192.168.1.13>
2019-05-27 20:57:53 -0500

    solucion5

---
+++ preg5/src/preg5/parcial_5.java
@@ -0,0 +1,25 @@
+package preg5;
+import java.util.Scanner;
+public class parcial_5 {
+	public static void main(String[] args) {
+	Scanner entrada = new Scanner(System.in);
+	String palabra="",nuevo="";
+	int j;
+	System.out.println("Ingrese la palabra deseada");
+
+	palabra=entrada.nextLine();
+	
+	j=palabra.length();
+	char al[]=new char[j];
+	for(int i=0;i<j;i++) {
+		al[j-i-1]=palabra.charAt(i);
+	}
+	nuevo=String.valueOf(al);
+	if(nuevo.equalsIgnoreCase(palabra)) {
+		System.out.print("Es palíndromo");
+		}
+	else {
+		System.out.print("No es palíndromo");
+	}
+}
+}
